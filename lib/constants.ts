63d4ed310763810ff5f5542b6d20b540c5da5e6f
System Administrator <root@MACs-MBP.lan>
2026-04-21 13:11:29 +0100

    Add RC prefix to CAC number

--- lib/constants.ts
+++ lib/constants.ts
@@ -9,7 +9,7 @@ export const COMPANY_INFO = {
   phoneRaw: "2348037415509",
   whatsappMessage: "Hello Swift Professional Solutions, I'd like to enquire about your services.",
   address: "House 12b Paradise Estate, Katampe, Abuja, Nigeria",
-  rcNumber: "8368089",
+  rcNumber: "RC-8368089",
   foundedYear: 2026,
 };
 
